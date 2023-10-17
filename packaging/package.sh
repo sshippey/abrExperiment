b06fb211416e14a348010ba46447a4d8dc84d207
Sam <weathercontrol@tuta.io>
2023-10-17 11:47:24 -0700

    Clean up and change interface to package.sh

--- packaging/package.sh
+++ packaging/package.sh
@@ -2,9 +2,15 @@
 
 # in=$2/out-$1-250k-480.mp4,stream=audio,output=$3/audio.mp4 \
 
-packager --segment_duration 2 \
-in=$2/out-$1-250k-480.mp4,stream=video,output=$3/h264_480p.mp4 \
-in=$2/out-$1-1M-720.mp4,stream=video,output=$3/h264_720.mp4 \
-in=$2/out-$1-6M-1080.mp4,stream=video,output=$3/h264_1080p.mp4 \
-in=$2/out-$1-50M-4k.mp4,stream=video,output=$3/h264_4k.mp4 \
---mpd_output $3/h264.mpd
+packager --segment_duration $2 \
+in=$1-profile1.mp4,stream=video,output=$1-profile1.mp4 \
+in=$1-profile2.mp4,stream=video,output=$1-profiler.mp4 \
+in=$1-profile3.mp4,stream=video,output=$1-profile3.mp4 \
+in=$1-profile4.mp4,stream=video,output=$1-profile4.mp4 \
+in=$1-profile5.mp4,stream=video,output=$1-profile5.mp4 \
+in=$1-profile6.mp4,stream=video,output=$1-profile6.mp4 \
+in=$1-profile7.mp4,stream=video,output=$1-profile7.mp4 \
+in=$1-profile8.mp4,stream=video,output=$1-profile8.mp4 \
+in=$1-profile9.mp4,stream=video,output=$1-profile9.mp4 \
+in=$1-profile0.mp4,stream=video,output=$1-profile0.mp4 \
+--mpd_output $1-h264.mpd
